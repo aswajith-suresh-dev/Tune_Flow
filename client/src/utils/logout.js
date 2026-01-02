acd8f288ec315faa01f5c834195be11f8159d673
Aswajith <aswajithsuresh2006@gmail.com>
2026-01-02 21:44:43 +0530

    logout fix

--- client/src/utils/logout.js
+++ client/src/utils/logout.js
@@ -2,6 +2,10 @@ export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("role");
   localStorage.removeItem("userId");
-
+const handleLogout = () => {
+  localStorage.clear();
+  setRole(null);
+  window.location.href = "/login";
+};
   window.location.href = "/login";
 };
